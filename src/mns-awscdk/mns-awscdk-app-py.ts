ea1b5b4c01da247c55cce81fd65047d569031d15
mnoumanshahzad <mnoumanshahzad@hotmail.com>
2022-06-09 23:36:05 +0200

    fix: generate sample readme and contrubuting guide

--- src/mns-awscdk/mns-awscdk-app-py.ts
+++ src/mns-awscdk/mns-awscdk-app-py.ts
@@ -11,50 +11,45 @@ export class MnsAwsCdkPythonApp extends AwsCdkPythonApp {
   constructor(options: AwsCdkPythonAppOptions) {
     super({
       ...options,
-      readme: {},
+      readme: {
+        contents: [
+          '# Project Name',
+          '',
+          '## Overview',
+          '',
+          '## Getting Started',
+          '',
+          '## Contributing',
+          '',
+          '## Related Projects',
+        ].join('\n'),
+      },
     });
 
-    this.generateSampleReadmeFile();
     this.generateSampleContributingFile();
   }
 
-  private generateSampleReadmeFile() {
-    const contents = `
-    # ${this.name}
-    
-    ## Overview
-    
-    ## Getting Started
-
-    ## Contributing
-
-    ## Related Projects
-    `.trim();
-    new SampleFile(this, 'README.md', {
-      contents: contents,
-    });
-  }
-
   private generateSampleContributingFile() {
-    const contents = `
-    # Welcome to contributing guide
+    const contents = [
+      '# Welcome to contributing guide',
+      '',
+      '## Getting Started',
+      '',
+      '## Pull Request Process',
+      '',
+      '## Code of Conduct',
+      '',
+      '## How to develop',
+      '',
+      '### Prerequisites',
+      '',
+      '### Main Concepts',
+      '',
+      '### Developing',
+      '',
+      '### Troubleshooting',
+    ].join('\n');
 
-    ## Getting Started
-
-    ## Pull Request Process
-
-    ## Code of Conduct
-
-    ## How to develop
-    
-    ### Prerequisites
-
-    ### Main Concepts
-
-    ### Developing
-
-    ### Troubleshooting
-    `.trim();
     new SampleFile(this, 'CONTRIBUTING.md', {
       contents: contents,
     });
