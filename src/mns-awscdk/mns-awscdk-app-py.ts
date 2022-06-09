80430755947ce4a370db639eb3f323ddc7bd3e38
mnoumanshahzad <mnoumanshahzad@hotmail.com>
2022-06-09 23:21:45 +0200

    feat: add sample readme and contrubuting guide

--- src/mns-awscdk/mns-awscdk-app-py.ts
+++ src/mns-awscdk/mns-awscdk-app-py.ts
@@ -1,3 +1,4 @@
+import { SampleFile } from 'projen';
 import { AwsCdkPythonApp, AwsCdkPythonAppOptions } from 'projen/lib/awscdk';
 
 /**
@@ -10,6 +11,52 @@ export class MnsAwsCdkPythonApp extends AwsCdkPythonApp {
   constructor(options: AwsCdkPythonAppOptions) {
     super({
       ...options,
+      readme: {},
+    });
+
+    this.generateSampleReadmeFile();
+    this.generateSampleContributingFile();
+  }
+
+  private generateSampleReadmeFile() {
+    const contents = `
+    # ${this.name}
+    
+    ## Overview
+    
+    ## Getting Started
+
+    ## Contributing
+
+    ## Related Projects
+    `.trim();
+    new SampleFile(this, 'README.md', {
+      contents: contents,
+    });
+  }
+
+  private generateSampleContributingFile() {
+    const contents = `
+    # Welcome to contributing guide
+
+    ## Getting Started
+
+    ## Pull Request Process
+
+    ## Code of Conduct
+
+    ## How to develop
+    
+    ### Prerequisites
+
+    ### Main Concepts
+
+    ### Developing
+
+    ### Troubleshooting
+    `.trim();
+    new SampleFile(this, 'CONTRIBUTING.md', {
+      contents: contents,
     });
   }
 }
